7a0f5d2a9e6bb9939448b4206ebe5cb44ffbd669
Talhakazmii <141968143+Talhakazmii@users.noreply.github.com>
2026-05-11 19:19:28 +0500

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var words = strs.GroupBy(x=>CreateKey(x)).ToDictionary(c=>c.Key,c=>c.ToList());
+        if(strs.Length == 1){
+            return  [[strs[0]]];
+        }
+        List<List<string>> lstToReturn = new List<List<string>>();
+        foreach(var pair in words){
+            lstToReturn.Add(pair.Value);
+        }
+        return lstToReturn;
+    }
+
+   public string CreateKey(string word){
+       int[] wordArr = new int[26]; 
+       StringBuilder stringToReturn = new StringBuilder();
+       foreach(var c in word){
+          wordArr[c -'a']++;
+       }
+       
+       for(int i = 0; i < wordArr.Length; i++){
+         if(wordArr[i] > 0){
+            stringToReturn.Append($"{(char)(i + 'a')}{wordArr[i]}#");
+           
+         }
+       }
+       return stringToReturn.ToString();
+     
+   }
+}
