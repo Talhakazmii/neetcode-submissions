45b03894225fb8fc8999edefd609aad39fed299a
Talhakazmii <141968143+Talhakazmii@users.noreply.github.com>
2026-09-12 09:02:55 +0500

    Add: sql-default-values - submission-2

---
+++ SQL/sql-default-values/submission-2.sql
@@ -0,0 +1,28 @@
+
+Create Table videos(
+    id integer,
+    name Text default 'Untitled',
+    is_published boolean default false
+
+
+);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+INSERT INTO videos (id, name, is_published) 
+VALUES (1, 'My Video', true),
+       (2, 'Another Video', false);
+
+INSERT INTO videos (id)
+VALUES (3),
+       (4);
+
+INSERT INTO videos (name)
+VALUES ('Video with no ID');
+
+SELECT * FROM videos;
